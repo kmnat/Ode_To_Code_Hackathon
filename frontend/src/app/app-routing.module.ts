3891203b6fc4d118b60831f5dfc603625483c233
Tanvi Kale <ktanvarada@gmail.com>
2025-05-24 14:51:13 +0530

    feat: added basic domino game

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { HomeComponent } from './features/home/home.component';
+import { GameDominoComponent } from './features/games/game-domino/game-domino.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'domino', component: GameDominoComponent },
   { path: '', component: HomeComponent }, // Default route
 ];
 
